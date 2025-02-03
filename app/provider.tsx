6d1bf484607ce3faf938f128ea54883f447c7834
IsmoilovMax <maxmudjon2121@gmail.com>
2025-02-03 11:59:13 +0900

    feat: Deployment

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes"
 import { SessionProvider } from "next-auth/react";
+import { ThemeProviderProps } from "next-themes/dist/types";
 
 export function Provider({ children, ...props }: ThemeProviderProps) {
     return (
